de1a7c9e5a811b6720c1a2191680994d4a8554ff
Salohiddin17 <salohiddindilmatov9@gmail.com>
2021-04-02 16:37:31 +0500

    Create project

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}...`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
